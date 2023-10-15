0bebf2df366700a3f3e966085cdd0d58b84805ee
Polina Popova <popovapolina2133@gmail.com>
2023-10-15 21:42:59 +0300

    Add interpolateColor

--- src/AlgorithmBresenham/Bresenham.java
+++ src/AlgorithmBresenham/Bresenham.java
@@ -3,15 +3,17 @@ package AlgorithmBresenham;
 import java.awt.*;
 
 public class Bresenham {
-    private final Color c;
     private int x1, x2, y1, y2; // координаты начала и конца
+    private Color sC,eC;
 
-    public Bresenham(int x1, int x2, int y1, int y2, Color c) {
+
+    public Bresenham(int x1, int x2, int y1, int y2, Color sC, Color eC) {
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
-        this.c = c;
+        this.sC = sC;
+        this.eC = eC;
     }
 
     public void draw(Graphics2D g) {
@@ -35,6 +37,9 @@ public class Bresenham {
                     y += ddy;
                     e += dx;
                 }
+                float t = (float) (x - x1) / (x2 - x1);
+                Color currentColor = interpolateColor(sC, eC, t);
+                g.setColor(currentColor);
                 g.fillRect(x, y, 1, 1);
             }
         } else {
@@ -47,11 +52,20 @@ public class Bresenham {
                     x += ddx;
                     e += dy;
                 }
+                float t = (float) (y - y1) / (y2 - y1);
+                Color currentColor = interpolateColor(sC, eC, t);
+                g.setColor(currentColor);
                 g.fillRect(x, y, 1, 1);
             }
         }
     }
     private int sign(int x) {
         return (x > 0) ? 1 : (x < 0) ? -1 : 0;
+    }
+    private Color interpolateColor(Color start, Color end, float t) {
+        int red = (int) (start.getRed() + t * (end.getRed() - start.getRed()));
+        int green = (int) (start.getGreen() + t * (end.getGreen() - start.getGreen()));
+        int blue = (int) (start.getBlue() + t * (end.getBlue() - start.getBlue()));
+        return new Color(red, green, blue);
     }
 }
